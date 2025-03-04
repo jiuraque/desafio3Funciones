fa1fe0a51515daf192985bf4c0a0383410cfda10
jiuraque <jiulianna.raquel@gmail.com>
2025-03-03 23:45:14 -0300

    se hace el html y el script del ejercicio de teclas

---
+++ 5_letras/assets/js/script.js
@@ -0,0 +1,26 @@
+const keyDiv = document.getElementById("key");
+const newDiv = document.createElement("div");
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "a") {
+    keyDiv.style.backgroundColor = "pink";
+  } else if (event.key === "s") {
+    keyDiv.style.backgroundColor = "orange";
+  } else if (event.key === "d") {
+    keyDiv.style.backgroundColor = "lightblue";
+  }
+
+  if (event.key === "q") {
+    crearNuevoDiv("purple");
+  } else if (event.key === "w") {
+    crearNuevoDiv("gray");
+  } else if (event.key === "e") {
+    crearNuevoDiv("brown");
+  }
+});
+
+function crearNuevoDiv(color) {
+  newDiv.classList.add("new-div");
+  newDiv.style.backgroundColor = color;
+  document.body.appendChild(newDiv);
+}
