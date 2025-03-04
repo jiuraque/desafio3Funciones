804add95718c7c20adebe45ba641ad3b5460ca67
jiuraque <jiulianna.raquel@gmail.com>
2025-03-03 21:11:23 -0300

    se hace el ejercicio de pintar el hello

---
+++ 3_ejercicio/assets/js/script.js
@@ -0,0 +1,7 @@
+const ele = document.getElementById("ele1");
+
+function pintar(color = "green") {
+    ele.style.backgroundColor = color;
+}
+
+ele.addEventListener("click", () => pintar("yellow"));
